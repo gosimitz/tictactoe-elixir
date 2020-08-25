f97d87b0dccfc6971d175b7b69adeda23dd3ae54
gosimitz <gosimitz@users.noreply.github.com>
2020-08-24 19:33:49 -0600

    Initial mapping of game

--- lib/tictactoe.ex
+++ lib/tictactoe.ex
@@ -12,7 +12,22 @@ defmodule Tictactoe do
       :world
 
   """
-  def hello do
-    :world
+  # Handle input (x,y)
+  # Check if spot occupied. If so, return error and ask for input. Display board
+  # if not, place item in spot.
+  # Check win conditions,
+  # ask for input
+  def hello(name) do
+    "Hello, #{name}!!!"
   end
+  def verify(item) do
+    IO.puts "it worked #{item}!"
+  end
+  # IO.puts Tictactoe.hello("Gus")
+
+  spot = IO.read(:line)
+  |> IO.puts
+
+  #|> Tictactoe.verify()
+
 end
